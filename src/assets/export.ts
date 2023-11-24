7dc16b5e534acdd36b8e6e48c776d5678cddf97d
skyszinho <127794316+skyszinho@users.noreply.github.com>
2023-11-24 16:52:22 -0300

    .

--- src/assets/export.ts
+++ src/assets/export.ts
@@ -5,6 +5,7 @@ import publistory from "./publistory.png";
 import corte from "./corte.jpg";
 import luzes from "./Luzes.jpeg";
 import barboterapia from "./Barboterapia.jpeg";
+
 export {
   GetInTouchCard,
   OpeningCard,
