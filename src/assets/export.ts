3a74418587996d92396ebf98092b50833ecb0279
skyszinho <127794316+skyszinho@users.noreply.github.com>
2023-11-24 17:02:28 -0300

    vercel error

--- src/assets/export.ts
+++ src/assets/export.ts
@@ -2,7 +2,7 @@ import GetInTouchCard from "./GetInTouchCard.svg";
 import OpeningCard from "./OpeningCard.svg";
 import logoo from "./logo.png";
 import publistory from "./publistory.png";
-import corte from "./corte.jpg";
+import corte from "./CortePadrao.jpg";
 import luzes from "./Luzes.jpeg";
 import barboterapia from "./Barboterapia.jpeg";
 
